038864fb5a5fdb5af8854374e7ed020e3735a92a
Louis <louishwy@heweiyongdeMacBook-Pro.local>
2018-01-09 01:29:26 +0800

    feat order server

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,8 @@ router.use('/', require('./users'));
 router.use('/profiles', require('./profiles'));
 router.use('/articles', require('./articles'));
 router.use('/tags', require('./tags'));
+router.use('/order',require('./make-order'));
+router.use('/ad',require('./ad'));
 
 router.use(function(err, req, res, next){
   if(err.name === 'ValidationError'){
