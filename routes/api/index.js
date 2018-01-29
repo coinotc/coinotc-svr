bb9f940795af5f657312acfe290b42e425c0f94d
shetz163 <shetz163@qq.com>
2018-01-29 12:40:14 +0800

    chore. created dashboard's api

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,6 +6,7 @@ router.use('/articles', require('./articles'));
 router.use('/tags', require('./tags'));
 router.use('/order',require('./make-order'));
 router.use('/guanggao',require('./ad'));
+router.use('/admin-dashboard',require('./admin-dashboard'));
 
 router.use(function(err, req, res, next){
   if(err.name === 'ValidationError'){
