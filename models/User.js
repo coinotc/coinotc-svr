5082bab58260b9a4111e891a9590d5139cd667f1
louishwy <905615218@qq.com>
2018-03-19 14:20:55 +0800

    save deviceToken in user

--- models/User.js
+++ models/User.js
@@ -4,52 +4,77 @@ var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 var secret = require('../config').secret;
 
-var UserSchema = new mongoose.Schema({
-  username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
-  email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
-  bio: String,
-  image: String,
-  orderCount: Number,
-  goodCount: Number,
-  volume:String,
-  logoutDateTime: Date,
-  idCard : String,
-  verifyName :String,
-  verify:Number,
-  phone:Number,
-  tradePrd:Number,
-  following: Array,
-  block:Array,
-  hash: String,
-  salt: String,
-  baseCurrency: String
-}, { timestamps: true });
+var UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+      index: true
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, 'is invalid'],
+      index: true
+    },
+    bio: String,
+    image: String,
+    orderCount: Number,
+    goodCount: Number,
+    volume: String,
+    logoutDateTime: Date,
+    idCard: String,
+    verifyName: String,
+    verify: Number,
+    phone: Number,
+    tradePrd: Number,
+    following: Array,
+    block: Array,
+    hash: String,
+    salt: String,
+    baseCurrency: String,
+    deviceToken: String
+  },
+  { timestamps: true }
+);
 
 UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
-UserSchema.methods.validPassword = function (password) {
-  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+    .toString('hex');
   return this.hash === hash;
 };
 
-UserSchema.methods.setPassword = function (password) {
+UserSchema.methods.setPassword = function(password) {
   this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+    .toString('hex');
 };
 
-UserSchema.methods.generateJWT = function () {
+UserSchema.methods.generateJWT = function() {
   var today = new Date();
   var exp = new Date(today);
   exp.setDate(today.getDate() + 60);
 
-  return jwt.sign({
-    id: this._id,
-    username: this.username,
-    exp: parseInt(exp.getTime() / 1000),
-  }, secret);
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      exp: parseInt(exp.getTime() / 1000)
+    },
+    secret
+  );
 };
 
-UserSchema.methods.toAuthJSON = function () {
+UserSchema.methods.toAuthJSON = function() {
   return {
     username: this.username,
     email: this.email,
@@ -58,28 +83,29 @@ UserSchema.methods.toAuthJSON = function () {
     image: this.image,
     orderCount: this.orderCount,
     goodCount: this.goodCount,
-    block : this.block,
+    block: this.block,
     following: this.following,
     nativeCurrency: this.baseCurrency
   };
 };
 
-UserSchema.methods.toProfileJSONFor = function (user) {
+UserSchema.methods.toProfileJSONFor = function(user) {
   return {
     username: this.username,
     bio: this.bio,
-    image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
+    image:
+      this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
     following: user ? user.isFollowing(this._id) : false
   };
 };
 
-UserSchema.methods.logout = function () {
+UserSchema.methods.logout = function() {
   this.logoutDateTime = new Date();
   return this.save();
 };
 
-UserSchema.methods.isFollowing = function (id) {
-  return this.following.some(function (followId) {
+UserSchema.methods.isFollowing = function(id) {
+  return this.following.some(function(followId) {
     return followId.toString() === id.toString();
   });
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,188 +4,225 @@ var passport = require('passport');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
-router.get('/user', auth.required, function(req, res, next){
-  User.findById(req.payload.id).then(function(user){
-    if(!user){ return res.sendStatus(401); }
+router.get('/user', auth.required, function(req, res, next) {
+  User.findById(req.payload.id)
+    .then(function(user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
 
-    return res.json({user: user.toAuthJSON()});
-  }).catch(next);
+      return res.json({ user: user.toAuthJSON() });
+    })
+    .catch(next);
 });
 router.patch('/users/public/follow', (req, res) => {
-  console.log(req.body)
-  console.log(req.query)
-  User.findOneAndUpdate({ username: req.query.username},{ following: req.body }, { new: true },  (err, result) => {
-    if (err) res.status(500).json(err);
-    res.status(201).json(result);
-  })
+  console.log(req.body);
+  console.log(req.query);
+  User.findOneAndUpdate(
+    { username: req.query.username },
+    { following: req.body },
+    { new: true },
+    (err, result) => {
+      if (err) res.status(500).json(err);
+      res.status(201).json(result);
+    }
+  );
 });
 
 router.patch('/users/public/tradepassword', (req, res) => {
-  console.log(req.body)
-  console.log(req.query)
-  User.findOneAndUpdate({ username: req.query.username},{ tradePrd: req.body.tradePrd },  (err, result) => {
-    if (err) res.status(500).json(err);
-    res.status(201).json(result);
-  })
+  console.log(req.body);
+  console.log(req.query);
+  User.findOneAndUpdate(
+    { username: req.query.username },
+    { tradePrd: req.body.tradePrd },
+    (err, result) => {
+      if (err) res.status(500).json(err);
+      res.status(201).json(result);
+    }
+  );
 });
-router.put('/user', auth.required, function(req, res, next){
-  console.log(req.body.user)
-  User.findById(req.payload.id).then(function(user){
-    if(!user){ return res.sendStatus(401); }
+router.put('/user', auth.required, function(req, res, next) {
+  console.log(req.body.user);
+  User.findById(req.payload.id)
+    .then(function(user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
 
-    // only update fields that were actually passed...
-    if(typeof req.body.user.username !== 'undefined'){
-      user.username = req.body.user.username;
-    }
-    if(typeof req.body.user.email !== 'undefined'){
-      user.email = req.body.user.email;
-    }
-    if(typeof req.body.user.bio !== 'undefined'){
-      user.bio = req.body.user.bio;
-    }
-    if(typeof req.body.user.image !== 'undefined'){
-      user.image = req.body.user.image;
-    }
-    if(typeof req.body.user.password !== 'undefined'){
-      user.setPassword(req.body.user.password);
-    }
-    if(typeof req.body.user.goodCount !== 'undefined'){
-      user.goodCount = req.body.user.goodCount;
-    }
-    if(typeof req.body.user.orderCount !== 'undefined'){
-      user.orderCount = req.body.user.orderCount;
-    }
-    if(typeof req.body.user.idCard !== 'undefined'){
-      user.idCard = req.body.user.idCard;
-    }
-    if(typeof req.body.user.verifyName !== 'undefined'){
-      user.verifyName = req.body.user.verifyName;
-    }
-    if(typeof req.body.user.phone !== 'undefined'){
-      user.phone = req.body.user.phone;
-    }
-    if(typeof req.body.user.tradePrd !== 'undefined'){
-      user.tradePrd = req.body.user.tradePrd;
-    }
-    console.log(user)
-    return user.save().then(function(){
-      return res.json({user: user.toAuthJSON()});
-    });
-  }).catch(next);
-});
-
-router.put('/users/base-currency', auth.required, function(req, res, next){
-  console.log(req.payload)
-  User.findById(req.payload.id).then(function(user){
-    if(!user){ return res.sendStatus(401); }
-
-    // only update fields that were actually passed...
-    if(typeof req.body.currency !== 'undefined'){
-      user.baseCurrency = req.body.currency;
-    }
-    
-    return user.save().then(function(){
-      return res.json({user: user.toAuthJSON()});
-    });
-  }).catch(next);
+      // only update fields that were actually passed...
+      if (typeof req.body.user.username !== 'undefined') {
+        user.username = req.body.user.username;
+      }
+      if (typeof req.body.user.email !== 'undefined') {
+        user.email = req.body.user.email;
+      }
+      if (typeof req.body.user.bio !== 'undefined') {
+        user.bio = req.body.user.bio;
+      }
+      if (typeof req.body.user.image !== 'undefined') {
+        user.image = req.body.user.image;
+      }
+      if (typeof req.body.user.password !== 'undefined') {
+        user.setPassword(req.body.user.password);
+      }
+      if (typeof req.body.user.goodCount !== 'undefined') {
+        user.goodCount = req.body.user.goodCount;
+      }
+      if (typeof req.body.user.orderCount !== 'undefined') {
+        user.orderCount = req.body.user.orderCount;
+      }
+      if (typeof req.body.user.idCard !== 'undefined') {
+        user.idCard = req.body.user.idCard;
+      }
+      if (typeof req.body.user.verifyName !== 'undefined') {
+        user.verifyName = req.body.user.verifyName;
+      }
+      if (typeof req.body.user.phone !== 'undefined') {
+        user.phone = req.body.user.phone;
+      }
+      if (typeof req.body.user.tradePrd !== 'undefined') {
+        user.tradePrd = req.body.user.tradePrd;
+      }
+      console.log(user);
+      return user.save().then(function() {
+        return res.json({ user: user.toAuthJSON() });
+      });
+    })
+    .catch(next);
 });
 
+router.put('/users/base-currency', auth.required, function(req, res, next) {
+  console.log(req.payload);
+  User.findById(req.payload.id)
+    .then(function(user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
 
-router.post('/users/login', function(req, res, next){
-  if(!req.body.user.email){
-    return res.status(422).json({errors: {email: "can't be blank"}});
+      // only update fields that were actually passed...
+      if (typeof req.body.currency !== 'undefined') {
+        user.baseCurrency = req.body.currency;
+      }
+
+      return user.save().then(function() {
+        return res.json({ user: user.toAuthJSON() });
+      });
+    })
+    .catch(next);
+});
+
+router.post('/users/login', function(req, res, next) {
+  if (!req.body.user.email) {
+    return res.status(422).json({ errors: { email: "can't be blank" } });
   }
 
-  if(!req.body.user.password){
-    return res.status(422).json({errors: {password: "can't be blank"}});
+  if (!req.body.user.password) {
+    return res.status(422).json({ errors: { password: "can't be blank" } });
   }
 
-  passport.authenticate('local', {session: true}, function(err, user, info){
-    if(err){ return next(err); }
+  passport.authenticate('local', { session: true }, function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
 
-    if(user){
+    if (user) {
+      user.deviceToken = req.body.deviceToken;
+      console.log(user);
       user.token = user.generateJWT();
-      return res.json({user: user.toAuthJSON()});
+      return res.json({ user: user.toAuthJSON() });
     } else {
       return res.status(422).json(info);
     }
   })(req, res, next);
 });
 
-router.post('/users', function(req, res, next){
+router.post('/users', function(req, res, next) {
   console.log('here');
   var user = new User();
-  user.verify = "0";
-  user.goodCount = "0";
-  user.orderCount = "0";
-  user.volume = "";
-  user.verifyName = "";
-  user.idCard = "";
-  user.phone = "";
-  user.tradePrd="";
+  user.verify = '0';
+  user.goodCount = '0';
+  user.orderCount = '0';
+  user.volume = '';
+  user.verifyName = '';
+  user.idCard = '';
+  user.phone = '';
+  user.tradePrd = '';
   user.following = [];
   user.block = [];
-  
+
+  user.deviceToken = req.body.deviceToken;
   user.username = req.body.user.username;
   user.email = req.body.user.email;
   user.setPassword(req.body.user.password);
-  
-  user.save().then(function(){
-    return res.json({user: user.toAuthJSON()});
-  }).catch(next);
+
+  user
+    .save()
+    .then(function() {
+      return res.json({ user: user.toAuthJSON() });
+    })
+    .catch(next);
 });
 
-router.get("/users/public", (req, res) => {
+router.get('/users/public', (req, res) => {
   let username = req.query.username;
-  
+
   console.log(req.query);
-  User.find({ username: `${username}`},'orderCount goodCount volume',(err, result) => {
-    if (err) {
+  User.find(
+    { username: `${username}` },
+    'orderCount goodCount volume deviceToken',
+    (err, result) => {
+      if (err) {
         res.status(500).send(err);
         return;
+      }
+      res.status(200).json(result);
     }
-    res.status(200).json(result);
-  });
+  );
 });
-router.get("/users/tradepassword", (req, res) => {
+router.get('/users/tradepassword', (req, res) => {
   let username = req.query.username;
-  
+
   console.log(req.query);
-  User.find({ username: `${username}`},'tradePrd',(err, result) => {
+  User.find({ username: `${username}` }, 'tradePrd', (err, result) => {
     if (err) {
-        res.status(500).send(err);
-        return;
+      res.status(500).send(err);
+      return;
     }
     res.status(200).json(result);
   });
 });
 router.patch('/users/public/block', (req, res) => {
-    console.log(req.body)
-    console.log(req.query)
-    User.findOneAndUpdate({ username: req.query.username},{ block: req.body }, { new: true },  (err, result) => {
+  console.log(req.body);
+  console.log(req.query);
+  User.findOneAndUpdate(
+    { username: req.query.username },
+    { block: req.body },
+    { new: true },
+    (err, result) => {
       if (err) res.status(500).json(err);
       res.status(201).json(result);
-    })
-  });
-
-  
-router.get("/users/logout", auth.required, function(req, res, next) {
-  console.log(req.payload.id);
-  let currentUser = req.user;
-  console.log(currentUser)
-  
-  User.findById(req.payload.id).then(function(user){
-    if (!user) { return res.sendStatus(401); }
-
-    return user.logout().then(function(){
-      res.json({logoutdatetime: new Date()});
-    });
-    req.logout(); // clears the passport session
-    req.session.destroy(); // destroys all session related data
-    
-  }).catch(next);
-  
+    }
+  );
 });
 
+router.get('/users/logout', auth.required, function(req, res, next) {
+  console.log(req.payload.id);
+  let currentUser = req.user;
+  console.log(currentUser);
+
+  User.findById(req.payload.id)
+    .then(function(user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      return user.logout().then(function() {
+        res.json({ logoutdatetime: new Date() });
+      });
+      req.logout(); // clears the passport session
+      req.session.destroy(); // destroys all session related data
+    })
+    .catch(next);
+});
 
 module.exports = router;
