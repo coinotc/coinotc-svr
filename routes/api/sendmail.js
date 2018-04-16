90d7a4f0629b19293f55d65144e50ad2dbaec844
Kenneth Phang <bunnyppl@gmail.com>
2018-04-17 06:59:26 +0800

    fix: every endpoint must return and json something else the middleware server will hang

--- routes/api/sendmail.js
+++ routes/api/sendmail.js
@@ -72,7 +72,7 @@ router.get(apiurl, function(req,res) {
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            return console.log(error);
+            res.status(500).json(err);
         }
         console.log('Message sent: %s', info.messageId);
         // Preview only available when sending through an Ethereal account
@@ -80,6 +80,7 @@ router.get(apiurl, function(req,res) {
 
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        res.status(200).json(info);
     });
 });
 
