b48199a6fdc23fb9042c4410e2b51c12361c0c4c
ChangJo <shetz163@qq.com>
2018-05-08 11:56:25 +0800

    feat. 2fa

--- routes/api/2fa.js
+++ routes/api/2fa.js
@@ -1,62 +1,59 @@
 var router = require('express').Router();
 var speakeasy = require("speakeasy");
 var mongoose = require('mongoose');
-var QRCode = require('qrcode');
+// var QRCode = require('qrcode');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
 const api = '/'
 
-router.get(api, auth.required,(req, res) => {
-    let user = req.query;
-    if (user) {
-        User.findOne({ username: user.username }, (err, result) => {
-            if (!result) {
-                res.status(500).send('error:username is invalid');
+router.get(api, auth.required, (req, res) => {
+    User.findById(req.payload.id, (err, result) => {
+        if (!result) {
+            res.status(500).json({ error: `username is invalid` });
+        } else {
+            if (result.tfa.effective) {
+                res.status(500).json({ error: `this user'2fa was effective` });
             } else {
-                if (result.tfa.effective) {
-                    res.status(500).send(`error:this user'2fa was effective`);
-                } else {
-                    let secret = speakeasy.generateSecret({ name: `Coinotc (${result.email})` });
-                    User.findOneAndUpdate({ username: user.username }, {
-                        tfa: { effective: false, secret: secret }
-                    }, (err, result) => {
-                        if (!err) {
-                            res.status(201).json(secret.base32);
-                            //res.send(secret.base32);
-                            //     QRCode.toDataURL(secret.otpauth_url, function (err, data_url) {
-                            //         // res.send(data_url);
-                            //         let img = `<img src='${data_url}' >`
-                            //         res.send(img);
-                            //     });
-                            // } else {
-                            //     res.status(500).send(err);
-                        }
-                    })
-                }
+                let secret = speakeasy.generateSecret({ otpauth_url: false });
+                User.findByIdAndUpdate(req.payload.id, {
+                    tfa: { effective: false, secret: secret }
+                }, (err, result) => {
+                    if (!err) {
+                        res.status(201).json(secret.base32);
+                        //res.send(secret.base32);
+                        //     QRCode.toDataURL(secret.otpauth_url, function (err, data_url) {
+                        //         // res.send(data_url);
+                        //         let img = `<img src='${data_url}' >`
+                        //         res.send(img);
+                        //     });
+                    } else {
+                        res.status(500).json(err);
+                    }
+                })
             }
-        })
-    } else {
-        res.status(500).send('error: no user');
-    }
+        }
+    })
 });
 
-router.post(api, auth.required,(req, res) => {
+router.post(api, auth.required, (req, res) => {
     let get = req.body;
-    User.findOne({ username: get.username }, (err, result) => {
-        if (!result.tfa.effective) {
+    User.findById(req.payload.id, (err, result) => {
+        if (result.validPassword(get.password)) {
             let match = speakeasy.totp.verify({
                 secret: result.tfa.secret.base32,
                 encoding: 'base32',
                 token: get.token
             });
             if (match) {
-                res.send('yes');
+                User.findByIdAndUpdate(req.payload.id, { tfa: { effective: true, secret: result.tfa.secret } }, (err, result) => {
+                    res.status(200).json(true);
+                })
             } else {
-                res.send('no');
+                res.status(500).json(false);
             }
         } else {
-            res.status(500).send(`error:this user'2fa is effective`)
+            res.status(500).json({ error: 'password is invalid' })
         }
     })
 })
