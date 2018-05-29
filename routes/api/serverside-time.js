1dcb5eae9d577baf76995c3356c6e5ecc9b1f060
Mr-Mooo <hjchoujiacheng@126.com>
2018-05-29 12:22:24 +0800

    fix serveside-time

--- routes/api/serverside-time.js
+++ routes/api/serverside-time.js
@@ -1,3 +1,4 @@
+
 var mongoose = require('mongoose');
 var router = require('express').Router();
 var moment = require('moment');
@@ -5,9 +6,10 @@ var moment = require('moment');
 const apiurl = '/';
 
 router.get("/serverside-time", (req,res)=>{
-    var durationFromLocalTime =  moment.duration(moment(moment.utc()).diff(moment()));
+    var durationFromLocalTime =  moment.duration(moment(new Date(0)).diff(moment()));
+    //console.log(moment.utc().format())
     var offsetSeconds = durationFromLocalTime.asSeconds()
-    console.log(offsetSeconds)
+    console.log("offsetSeconds:"+offsetSeconds)
     res.status(201).json(offsetSeconds);
 })
 
