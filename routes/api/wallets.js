d1de98ac034811194f946bc8e50b26c3ef426a8d
Kenneth Phang <bunnyppl@gmail.com>
2018-06-08 01:37:15 +0800

    fix: monero NAN

--- routes/api/wallets.js
+++ routes/api/wallets.js
@@ -3,7 +3,9 @@ var router = require('express').Router();
 var passport = require('passport');
 var User = mongoose.model('User');
 var auth = require('../auth');
-var request = require('request')
+var request = require('request');
+var rp = require('request-promise');
+
 const walletApiKey = process.env.COINOTC_WALLET_API_KEY;
 const ApiUrl = process.env.COINOTC_WALLET_API_URL;
 const globalWalletPassword = process.env.COINOTC_GLOBAL_WALLET_PASSWORD;
@@ -49,7 +51,7 @@ router.get('/wallet-info', auth.required, function(req, res, next){
            
                 },
                 XMR: {
-                    address: data.monero.accInfo[1].result.address
+                    address: data.monero.accInfo[2].result.address
           
                 }
                 
@@ -64,6 +66,77 @@ router.get('/wallet-info', auth.required, function(req, res, next){
 
 router.post('/withdrawal', auth.required, function(req, res, next){
     console.log('withdrawal ...');
+    console.log(">>> transfer crypto >>> " + req.body);
+    console.log(">>> treq.payload.id >>> " + req.payload.id);
+    User.findById(req.payload.id)
+    .then(function (user) {
+        console.log(">>> user>>> " + user);
+        if (!user) {
+            return res.sendStatus(401);
+        }
+        console.log(">>> {req.body.type>>> " + req.body.type);
+        console.log(">>> {req.body.type>>> " + user.baseCurrency);
+        let userCurrency = user.baseCurrency;
+        if(typeof(user.baseCurrency) === 'undefined' ){
+            userCurrency = "USD";
+        }
+        console.log(">>> {req.body.type>>> " +userCurrency);
+        let idType = 0;
+        if(req.body.type.toUpperCase() === 'ETH'){
+            idType = 'ethereum';
+        }else if (req.body.type.toUpperCase() === 'XRP'){
+            idType = 'ripple';
+        }else if (req.body.type.toUpperCase() === 'ADA'){
+            idType = 'cardano';
+        }else if (req.body.type.toUpperCase() === 'XLM'){
+            idType = 'stellar';
+        }else if (req.body.type.toUpperCase() === 'XMR'){
+            idType = 'monero';
+        }
+
+        rp({ uri: `https://api.coinmarketcap.com/v1/ticker/${idType}/?convert=${userCurrency}`, json: true }).then((result) => {
+            console.log("result > " + result)
+            let equivAmount = parseInt(result) * parseInt(req.body.amount);
+            console.log(equivAmount);
+            var withdrawalPostBody = {
+                "cryptoCurrency": req.body.type.toUpperCase(),
+                "email": user.email,
+                "unit": req.body.amount,
+                "transactCurrency": user.baseCurrency,
+                "pin": user.tradePasswordHash,
+                "equivalentAmount": equivAmount,
+                "beneficiaryAddress": req.body.address,
+                "memo": req.body.notes
+            }
+            console.log('TRANSFER > ...' + withdrawalPostBody);
+            currentUserEmail = user.email;
+            var options = {
+                url: `${ApiUrl}transactions/withdrawal`,
+                headers : {
+                'Authorization': `Bearer ${walletApiKey}`,
+                'Origin': origin
+                },
+                body: withdrawalPostBody,
+                json:true
+            };
+
+            request.post(options, function(error, response, body){
+                if(error) res.status(500).send(error);
+                console.log(body);
+                let data = JSON.parse(body);
+                console.log(data)
+                return res.status(201).json(data);
+            })
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send(`err: ${err}`);
+        })
+
+        
+    })
+    .catch((err) => {
+        res.status(500).send(`err: ${err}`);
+    })
 });
 
 router.get('/balance/:id/:cryptoType', auth.required, function(req, res, next){
